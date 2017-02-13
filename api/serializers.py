15567fb3a40b41c670f5051635896d6a58a5c570
Sargun Vohra <sargunster@gmail.com>
2017-02-13 16:42:03 -0500

    Allow user creation without logging in

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,26 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'is_staff')
+        fields = (
+            'url',
+            'id',
+            'username',
+            'email',
+            'is_staff',
+            'first_name',
+            'last_name',
+            'date_joined',
+            'password',
+        )
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'id': {'read_only': True},
+            'date_joined': {'read_only': True},
+            'is_staff': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        user = super(UserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
