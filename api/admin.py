a8a63fe2846a91af7d447d343438a3a22c04994c
Sargun Vohra <sargunster@gmail.com>
2017-02-23 22:49:31 -0500

    Admin models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from api import models
 
-# Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+
+class MembershipAdmin(admin.ModelAdmin):
+    pass
+
+
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Room, RoomAdmin)
+admin.site.register(models.Message, MessageAdmin)
+admin.site.register(models.Membership, MembershipAdmin)
+admin.site.register(models.Like, LikeAdmin)
